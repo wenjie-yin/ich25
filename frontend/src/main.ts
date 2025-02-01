64a39c9ebe77e47bb21d83f2f7ef70a264402d15
Boyuan Jiang <jiangby03@sina.com>
2025-02-01 19:05:58 +0000

    login page and chat page

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import TDesign from 'tdesign-vue-next';
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import 'tdesign-vue-next/es/style/index.css';
+
+createApp(App).use(router).use(TDesign).mount('#app')
