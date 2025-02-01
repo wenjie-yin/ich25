edfa3ccb73d58b1de04b2988f5e012dd08227274
jt2121 <jt2121@ic.ac.uk>
2025-02-01 17:22:15 +0000

    add skeletons for user interface

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,4 +3,53 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { VBtn, VTextField, VForm, VContainer, VRow, VCol, VCard, VCardTitle, VCardText, VCardActions, VTextarea, VIcon, VApp, VMain, VAppBar } from 'vuetify/components'
+import { mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+    components: {
+        VBtn,
+        VTextField,
+        VForm,
+        VContainer,
+        VRow,
+        VCol,
+        VCard,
+        VCardTitle,
+        VCardText,
+        VCardActions,
+        VTextarea,
+        VIcon,
+        VApp,
+        VMain,
+        VAppBar
+    },
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        sets: {
+            mdi,
+        },
+    },
+    theme: {
+        themes: {
+            light: {
+                colors: {
+                    primary: '#10a37f',
+                    secondary: '#0e906f',
+                },
+            },
+        },
+    },
+})
+
+const app = createApp(App)
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
