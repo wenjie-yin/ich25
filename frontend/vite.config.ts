17a4abfd9990fc3868f23658e2d0b6b20850b899
Boyuan Jiang <jiangby03@sina.com>
2025-02-01 22:33:36 +0000

    api address fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     // Add proxy configuration
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // Your backend port
+        target: 'http://0.0.0.0:8000', // Your backend port
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
