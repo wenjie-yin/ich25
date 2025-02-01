23af3a13cfdb76aeaf28a1f65b926a3ddf44981a
Boyuan Jiang <jiangby03@sina.com>
2025-02-01 17:48:25 +0000

    Revert "add skeletons for user interface"
    This reverts commit 5921eae4a39eff45277811ce1b2c67b794b220af.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,36 +1,15 @@
 <template>
-  <v-app>
-    <v-main class="main-content">
-      <router-view></router-view>
-    </v-main>
-  </v-app>
+  <div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script setup lang="ts">
-// Component logic here
 </script>
 
-<style>
-html, body {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 100%;
-  overflow: hidden;
-}
-
-.main-content {
-  width: 100%;
-  height: 100vh;
-  max-width: 100vw;
-}
-
-.v-application {
-  width: 100vw;
-  min-height: 100vh;
-}
-
-:root {
-  --v-layout-top: 0px !important;
+<style scoped>
+nav {
+  display: flex;
+  gap: 1em;
 }
 </style>
