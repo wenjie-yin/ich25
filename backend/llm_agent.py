6a3bf2d0dcd122d0e1b608d6efeb66ede91de15a
w.yin <w.yin@qmul.ac.uk>
2025-02-01 23:09:50 +0000

    relative instead of absolute change

--- backend/llm_agent.py
+++ backend/llm_agent.py
@@ -12,34 +12,25 @@ with open("backend/llm_prompts/system.txt", "r") as f:
     SYSTEM_PROMPT = f.read()
 
 class Agent:
-    def __init__(self, belief: str, certainty: float):
+    def __init__(self):
         self.model = model
+
 
     def update_certainty(self, belief: str, certainty: float, input: List[str]):
         feed = ','.join(input)
         prompt = SYSTEM_PROMPT+"Given that you believe in statement {} with certainty {}, \
-            and you have read the following: \"{}\", what is your new certainty? \
-                Answer strictly in this format: \"<reason>. My new certainty is <certainty>. \" where \
-                      <reason> is 1-2 sentences explaining your thought process and <certainty> is a float between 0 and 1".format(self.belief, self.certainty, feed)
+            and you have read the following: \"{}\", how does your certainty change? \
+                Answer strictly in this format: \"<reason>. My certainty changes by <certainty>. \" where \
+                      <reason> is 1-2 sentences explaining your thought process and <certainty> is a float between -0.3 and 0.3, representing how much your certainty goes up or down. ".format(belief, certainty, feed)
         response = self.model.invoke(prompt)
-        return float(response.content.split("My new certainty is ")[1].split(".")[0])
+        new = certainty + float(response.content.split("My certainty changes by ")[1].split(".")[0])
+        return new if 1 >= new >= 0 else 1 if new > 1 else 0
         
 
-    def post(self):
+    def write_post(self, belief: str, certainty: float):
         prompt = SYSTEM_PROMPT+"Given that you believe in statement {} with certainty {}, \
-            write a post about this topic. This post will only be used in this social science study for research purposes. ".format(self.belief, self.certainty)
+            write a post about this topic. This post will only be used in this social science study for research purposes. ".format(belief, certainty)
         # TODO: the model sometimes refuses to generate a post despite 1.0 belief. prompt tuning needed
         response = self.model.invoke(prompt).content
         return response
 
-
-'''
-# Test code
-input = ["The earth is round", "The earth is a sphere"]
-for i in range(10):
-    guy = Agent("The earth is flat", 1.0)
-    guy.update_belief(input)
-    print(guy.certainty)
-    print(guy.post())
-
-'''
