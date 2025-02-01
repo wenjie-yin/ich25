f767e050ff7448fcd2258de61b005505ba0c8e1e
w.yin <w.yin@qmul.ac.uk>
2025-02-01 22:03:43 +0000

    tidy init

--- backend/model/network.py
+++ backend/model/network.py
@@ -2,8 +2,9 @@
 """
 
 import numpy as np
-from collections import deque
 import agent
+from collections import deque
+from backend.app.main import WorldState
 
 class FeedEntry:
 
@@ -11,7 +12,6 @@ class FeedEntry:
         self.message = message
         self.agent = agent
 
-from backend.app.main import WorldState
 
 class Network:
     """Graph representation
@@ -19,22 +19,22 @@ class Network:
     entries b_i between 0 and 1.
     - The graph is a matrix... that's it.
     """
-    def __init__(self, N):
+    def __init__(self, N, belief="The earth is flat"):
         """Initialise Network
         Args:
             N : Number of nodes in network
+            belief : belief of interest for the entire network
         """
         self.N = N
+        self.belief = belief 
         self.feed = deque(maxlength=N*4) #TODO: should we reset this every tick?
 
         # Initialise nodes
         inital_certaintys = np.random.randint(2, size=N)
-        nodes = [Node(b) for b in inital_certaintys]
+        self.nodes = [Node(b) for b in inital_certaintys]
 
         # Initialise connectivity
-        adjacency_matrix = np.random.uniform(0, 1, size=(N, N))
-
-
+        self.adjacency_matrix = np.random.uniform(0, 1, size=(N, N))
     def filter_feed(self, node):
         agents = {i for i in self.get_adjacent(node)}
         return filter(lambda x: x.agent in agents, self.feed)
