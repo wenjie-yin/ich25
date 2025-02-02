b4d5b3f7c18b4be7673f8e7dfc55ee93210be384
w.yin <w.yin@qmul.ac.uk>
2025-02-02 01:16:37 +0000

    new llm tests

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import backend.llm_agent
+from backend.llm_agent import Agent
 
-llm_agent = llm_agent.Agent()
+llm_agent = Agent()
 
 print(llm_agent.update_certainty("COVID-19 vaccine is dangerous", 0.5, ["mRNA was developed too quickly", "Vaccines are very well tested", "Ivermectin is a safer alternative"]))
 
